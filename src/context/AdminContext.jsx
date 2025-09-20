2cc10b6ec9c65e1577780706e18dfd52589ce7b4
subhasgh <subharathasiva@gmail.com>
2025-09-20 18:24:58 +0530

    Add AdminContext.jsx

---
+++ src/context/AdminContext.jsx
@@ -0,0 +1,17 @@
+// src/context/AdminContext.jsx
+import React, { createContext, useState } from "react";
+
+export const AdminContext = createContext();
+
+export const AdminProvider = ({ children }) => {
+  const [isAdminLoggedIn, setIsAdminLoggedIn] = useState(false);
+  const [adminData, setAdminData] = useState(null);
+
+  return (
+    <AdminContext.Provider
+      value={{ isAdminLoggedIn, setIsAdminLoggedIn, adminData, setAdminData }}
+    >
+      {children}
+    </AdminContext.Provider>
+  );
+};
